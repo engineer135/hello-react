434a2cb83cd0bedd1c9276410e125fd92dfe39a4
engineer135 <everafterk@naver.com>
2021-04-10 08:43:33 +0900

    이벤트 핸들링~

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import MyComponent from './Say'
+import MyComponent from './EventPractice'
 
 function App() {
   return <MyComponent>나는 칠드런</MyComponent>
---
+++ src/EventPractice.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class EventPractive extends Component {
+    state = {
+        message : ''
+    }
+    render(){
+        return(
+            <div>
+                <h1>이벤트연습ㅋ</h1>
+                <input type="text" name="message" placeholder="아무거나 입력해라"
+                value={this.state.message}
+                onChange={(e)=>{
+                    this.setState({message: e.target.value});
+                }}/>
+                미리보기:{this.state.message}<br/>
+                <button onClick={
+                    ()=>{
+                        alert(this.state.message);
+                        this.setState({
+                            message: ''
+                        });
+                    }
+                }>확인</button>
+            </div>
+        )
+    }
+}
+
+export default EventPractive;
