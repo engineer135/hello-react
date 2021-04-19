f7cb81589286549343cae96e9ad6b0cd72535550
engineer135 <everafterk@naver.com>
2021-04-20 05:24:06 +0900

    함수형 컴포넌트 useEffect

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Info = ()=>{
     const [name, setName] = useState('');
     const [nickName, setNickName] = useState('');
+
+    // useEffect : componentDidMount와 componentDidUpdate를 합친 것
+    // 컴포넌트가 렌더링될때마다 특정 작업 수행
+    // 두번째 인자로 []을 넘겨주면, 마운트될때만 수행
+    // 두번째 인자로 [name]을 넘겨주면, 해당 값이 변경됐을때만 수행
+    useEffect(()=>{
+        console.log('렌더링 완료!');
+        console.log({name, nickName});
+    }, [name]);
 
     const onChangeName = (e)=>{
         setName(e.target.value);
