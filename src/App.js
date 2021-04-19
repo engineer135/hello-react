6c376c0c393b8c06f70e1974f92a7628e445ffe4
engineer135 <everafterk@naver.com>
2021-04-20 05:17:59 +0900

    함수형 컴포넌트 useState 복수개 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './App.css';
-import MyComponent from './CounterFn'
+import MyComponent from './Info'
 
 const App = () => {
   return <MyComponent/>
