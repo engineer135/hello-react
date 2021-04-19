9e0c6d82cdad7503221275466d733adc467a5221
engineer135 <everafterk@naver.com>
2021-04-20 05:11:43 +0900

    함수형 컴포넌트 useState

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './App.css';
-import MyComponent from './LifeCycleSample'
-import ErrorBoundary from './ErrorBoundary'
+import MyComponent from './CounterFn'
 
-// 랜덤 색상 생성
-// 16777215 = 16진수로 ffffff
-function getRandomColor(){
-  return '#'+Math.floor(Math.random()*16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: '#000000'
-  }
-
-  handleClick = ()=>{
-    this.setState({
-      color: getRandomColor()
-    });
-  }
-
-  render(){
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상 예~</button>
-        <ErrorBoundary>
-          <MyComponent color={this.state.color}></MyComponent>
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <MyComponent/>
+};
 
 export default App;
