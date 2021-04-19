0ceb38c1fb97639762b8807ced234f917936d636
engineer135 <everafterk@naver.com>
2021-04-20 05:33:37 +0900

    함수형 컴포넌트 useEffect에 뒷정리 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import './App.css';
 import MyComponent from './Info'
 
 const App = () => {
-  return <MyComponent/>
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button onClick={()=>{setVisible(!visible);}}>{visible?'숨기기':'보이기'}</button>
+      <hr/>
+      {visible && <MyComponent/>}
+    </div>
+  );
 };
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -8,10 +8,15 @@ const Info = ()=>{
     // 컴포넌트가 렌더링될때마다 특정 작업 수행
     // 두번째 인자로 []을 넘겨주면, 마운트될때만 수행
     // 두번째 인자로 [name]을 넘겨주면, 해당 값이 변경됐을때만 수행
+    // 언마운트되기 전이나 업데이트되기 직전에 뭔가 수행하고 싶다면 뒷정리(cleanup) 함수 반환해주면 됨
     useEffect(()=>{
-        console.log('렌더링 완료!');
-        console.log({name, nickName});
-    }, [name]);
+        console.log('effect!');
+        console.log(name); // 이게 나중에 수행됨. 수정후 name
+        return ()=>{
+            console.log('cleanup');
+            console.log(name); // 이게 먼저 수행됨. 수정전 name
+        };
+    }, []);
 
     const onChangeName = (e)=>{
         setName(e.target.value);
