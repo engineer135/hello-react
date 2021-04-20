64e02c658b3ed58015ab3bf1fbc77564a5716421
engineer135 <everafterk@naver.com>
2021-04-20 21:32:50 +0900

    useMemo를 사용해 내용 타겟값이 변경됐을때만 함수 호출

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 const getAverage = (numbers) => {
     console.log('평균값 계산중...');
@@ -21,6 +21,9 @@ const Average = () => {
         setNumber('');
     };
 
+    // 이렇게 useMemo 함수를 호출하면서 getAverage를 첫번째 파라미터로 넘겨주고, 두번째 파라미터로 list를 넘겨주면 list 배열의 내용이 바뀔때만 getAverage 함수 호출함
+    const avg = useMemo(()=>getAverage(list), [list]);
+
     // 이렇게 하면 렌더링할때마다(인풋내용이 수정될때마다) 평균값 계산이 됨. 낭비.. 이를 막기 위해 useMemo Hook을 사용한다.
     return(
         <div>
@@ -32,7 +35,7 @@ const Average = () => {
                 ))}
             </ul>
             <div>
-                <b>평균값:</b>{getAverage(list)}
+                <b>평균값:</b>{avg}
             </div>
         </div>
     );
